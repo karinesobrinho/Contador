e9df45783b9e364235499e97167401cf93078317
karine sobrinho <karine.silva.sobrinho@gmail.com>
2020-09-29 18:25:50 -0300

    textos e alinhamento

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,23 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    title: 'Contador',
-    home: Container(color: Colors.blueGrey),
-  ));
+      title: 'Contador',
+      home: Column(
+        mainAxisAlignment: MainAxisAlignment.center, //alinhamento da coluna (vertical)
+        children: <Widget>[
+          Text(
+            'Pessoas: 0',
+            style: TextStyle(
+              color: Colors.white, 
+              fontWeight: FontWeight.bold),
+          ),
+          Text(
+            'pode entrar',
+            style: TextStyle(
+                color: Colors.pinkAccent,
+                fontSize: 30.0,
+                fontStyle: FontStyle.italic),
+          )
+        ],
+      )));
 }
